0a96ce23601cffa88c40cfbeada5f419e5ef5e08
janvda <jan.vandenaudenaerde@gmail.com>
2018-03-28 09:06:51 +0200

    Improved logging / restructured python program / get_status instead of status

--- examples/Python/snowboy_mqtt.py
+++ examples/Python/snowboy_mqtt.py
@@ -1,8 +1,24 @@
+##################################################################################
+#  logging settings  (must be put first otherwise becomes overwritten by other modules
+#  
+import logging
+
+# basicConfig must be done first as otherwise becomes overwritten by other imported modules
+logging.basicConfig(format='[%(asctime)s %(threadName)s, %(levelname)s] %(message)s',
+                    level=logging.DEBUG)
+										
+logging.info('Starting program...')
+
+####################################################################################
 import snowboydecoder_arecord
 import sys
-import signal
+
 import paho.mqtt.client as mqtt #import the client1
 
+####################################################################################
+#  interrupted block
+
+import signal
 interrupted = False
 
 def signal_handler(signal, frame):
@@ -12,63 +28,56 @@ def signal_handler(signal, frame):
 def interrupt_callback():
     global interrupted
     return interrupted
+		
+#####################################################################################
+#    MQTT stuff
 
-"""if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python demo.py your.model")
-    sys.exit(-1)
-
-model = sys.argv[1]"""
-
-# capture SIGINT signal, e.g., Ctrl+C
-#signal.signal(signal.SIGINT, signal_handler)
-
-# JVA 2018-01-25 MQTT stuff
-def on_message(client, userdata, message):
+def on_mqtt_message(client, userdata, message):
   global interrupted
-  print("message received " ,str(message.payload.decode("utf-8")))
-  print("message topic=",message.topic)
-  print("message qos=",message.qos)
-  print("message retain flag=",message.retain)
   cmd = str(message.payload.decode("utf-8"))
+  logging.info("mqtt rcvd=" + cmd + "[topic=" + message.topic + ",qos=" + str(message.qos) + ",retain=" + str(message.retain) + "]")
   if cmd == "status" :
-    mqtt_client.publish("pi3/aiy/snowboy/status", "interrupted" if interrupted else "listening" )
-  if cmd == "interrupt" :
+    mqtt_client.publish("pi3/aiy/snowboy/status", "stopped" if interrupted else "started" )
+  if cmd == "stop" :
     interrupted = True
-  if cmd == "listen" :
+  elif cmd == "start" :
     interrupted = False
-  print ("interrupted=", interrupted)
+  elif cmd != "get_status" :
+    logging.error("unexpected mqtt command (=" + cmd + ") received.  This command is not supported")
+    return
+  mqtt_client.publish("pi3/aiy/snowboy/status", "stopped" if interrupted else "started" )	
+  logging.debug ("interrupted="+str(interrupted))
 		
 global mqtt_client
 mqtt_broker_address="192.168.1.131" 
 mqtt_client = mqtt.Client("pi3_aiy_snowboy") #create new instance
-mqtt_client.on_message = on_message
+mqtt_client.on_message = on_mqtt_message
 mqtt_client.connect(mqtt_broker_address) #connect to broker
 mqtt_client.subscribe("pi3/aiy/snowboy/cmd",1)
 mqtt_client.loop_start()
 
-# capture SIGINT signal, e.g., Ctrl+C
-#signal.signal(signal.SIGINT, signal_handler)
-
-
+#####################################################################################
+#    Snowboy stuff 
+#       - specifying models, sensitivy and callback functions
+#
 models = [ "./resources/models/snowboy.umdl", 
            "./resources/models/stemija.pmdl" ]
 sensitivity = [0.5]*len(models)
 detector = snowboydecoder_arecord.HotwordDetector(models, sensitivity=sensitivity)
 
-
-# main loop
-print('Listening... Press Ctrl+C to exit')
 def hotword_detected():
-    """
-    """
+    logging.info('hotword_detected')
     mqtt_client.publish("pi3/aiy/snowboy/event_type","hotword_detected" )
-    print("hotword_detected")
+
 		
 callbacks = [lambda: hotword_detected(),
              lambda: hotword_detected()]
+						 
+logging.info('Starting main loop ...')						 
 detector.start(detected_callback=callbacks,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
+logging.info('Terminating program ...')
 detector.terminate()
+logging.info('Program terminated.')
